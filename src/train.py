dcf8098236098d40cc95fa396e872a933ab18ee1
VISHAKHA <vishakhab603@gmail.com>
2026-03-01 01:06:11 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,5 +1,41 @@
 # src/train.py
 
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+# Extract embeddings
+latent_model = model.get_layer("fusion_dense")  # or correct layer name
+latent_output = latent_model.output
+
+# Build embedding extractor
+from tensorflow.keras.models import Model
+embedding_model = Model(inputs=model.input, outputs=latent_output)
+
+embeddings = embedding_model.predict([Xb_test, Xp_test])
+
+pca = PCA(n_components=2)
+Z = pca.fit_transform(embeddings)
+
+plt.scatter(Z[:,0], Z[:,1], c=y_test)
+plt.title("HDM Latent Space")
+plt.show()
+
+from sklearn.metrics import confusion_matrix, roc_auc_score
+
+y_pred = model.predict([Xb_test, Xp_test])[0]
+y_pred_labels = y_pred.argmax(axis=1)
+
+cm = confusion_matrix(y_test.argmax(axis=1), y_pred_labels)
+print("Confusion Matrix:\n", cm)
+
+auc = roc_auc_score(y_test, y_pred)
+print("ROC AUC:", auc)
+
+def simulate_degradation(model, sequence):
+    preds = model.predict(sequence)
+    hli_scores = preds[1]
+    return hli_scores
+    
 from src.data_generator import generate_multimodal_data
 from src.preprocessing import preprocess_data
 from src.models import build_fusion_model

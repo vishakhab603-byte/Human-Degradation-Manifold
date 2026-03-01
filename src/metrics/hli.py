df1457642a4d235af016f76a42ad99444c2d989e
VISHAKHA <vishakhab603@gmail.com>
2026-03-01 14:54:35 +0530

    Refactor HLI computation, remove TensorFlow dependencies
    Removed TensorFlow model building and replaced with HLI computation.

--- src/metrics/hli.py
+++ src/metrics/hli.py
@@ -1,75 +1,24 @@
-
-import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import (
-    Input, Dense, Dropout, Concatenate,
-    LayerNormalization, MultiHeadAttention, Lambda
-)
-from tensorflow.keras.optimizers import Adam
+import numpy as np
+from scipy.spatial.distance import mahalanobis
 
 
-def build_attention_model(config):
+def compute_hli(X, baseline_end=400):
 
-    behavior_input = Input(shape=(2,), name="behavior_input")
-    physio_input = Input(shape=(2,), name="physio_input")
+    mean_vec = np.mean(X[:baseline_end], axis=0)
+    cov_matrix = np.cov(X[:baseline_end].T)
+    cov_matrix += np.eye(cov_matrix.shape[0]) * 1e-6
+    inv_cov = np.linalg.inv(cov_matrix)
 
-    x1 = Dense(config.dense_units, activation='relu')(behavior_input)
-    x1 = Dropout(config.dropout_rate)(x1)
-    z_behavior = Dense(32, activation='relu')(x1)
+    distances = [
+        mahalanobis(x, mean_vec, inv_cov)
+        for x in X
+    ]
 
-    x2 = Dense(config.dense_units, activation='relu')(physio_input)
-    x2 = Dropout(config.dropout_rate)(x2)
-    z_physio = Dense(32, activation='relu')(x2)
+    distances = np.array(distances)
 
-    z_behavior_exp = Lambda(lambda x: tf.expand_dims(x, axis=1))(z_behavior)
-    z_physio_exp = Lambda(lambda x: tf.expand_dims(x, axis=1))(z_physio)
-
-    attention = MultiHeadAttention(
-        num_heads=config.num_heads,
-        key_dim=16
-    )(
-        query=z_behavior_exp,
-        value=z_physio_exp,
-        key=z_physio_exp
+    tau = (
+        np.mean(distances[:baseline_end]) +
+        2 * np.std(distances[:baseline_end])
     )
 
-    attention = Lambda(lambda x: x[0] + x[1])(
-        [attention, z_behavior_exp]
-    )
-    attention = LayerNormalization()(attention)
-    attention = Lambda(lambda x: tf.squeeze(x, axis=1))(attention)
-
-    fusion = Concatenate()([z_behavior, z_physio, attention])
-    fusion = Dense(64, activation='relu')(fusion)
-    fusion = Dropout(config.dropout_rate)(fusion)
-
-    class_output = Dense(
-        config.num_classes,
-        activation='softmax',
-        name='classification'
-    )(fusion)
-
-    hli_output = Dense(
-        1,
-        activation='linear',
-        name='hli'
-    )(fusion)
-
-    model = Model(
-        inputs=[behavior_input, physio_input],
-        outputs=[class_output, hli_output]
-    )
-
-    model.compile(
-        optimizer=Adam(config.learning_rate),
-        loss={
-            'classification': 'categorical_crossentropy',
-            'hli': 'mse'
-        },
-        metrics={
-            'classification': 'accuracy',
-            'hli': 'mse'
-        }
-    )
-
-    return model
+    return distances, tau

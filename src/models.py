81218c58a604868d54c3e1b8172a9b3283436705
VISHAKHA <vishakhab603@gmail.com>
2026-03-01 14:08:55 +0530

    Refactor model layers and add Lambda operations

--- src/models.py
+++ src/models.py
@@ -1,8 +1,10 @@
 # src/models.py
 
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Dropout, Concatenate, LayerNormalization
-from tensorflow.keras.layers import MultiHeadAttention
+from tensorflow.keras.layers import (
+    Input, Dense, Dropout, Concatenate,
+    LayerNormalization, MultiHeadAttention, Lambda
+)
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
 
@@ -28,26 +30,27 @@ def build_fusion_model(
     x2 = Dropout(dropout_rate)(x2)
     z_physio = Dense(32, activation='relu')(x2)
 
-    from tensorflow.keras.layers import Lambda
+    # Expand dims using Lambda
+    z_behavior_exp = Lambda(lambda x: tf.expand_dims(x, axis=1))(z_behavior)
+    z_physio_exp = Lambda(lambda x: tf.expand_dims(x, axis=1))(z_physio)
 
-# Expand dims using Lambda
-z_behavior_exp = Lambda(lambda x: tf.expand_dims(x, axis=1))(z_behavior)
-z_physio_exp = Lambda(lambda x: tf.expand_dims(x, axis=1))(z_physio)
+    # Cross-modal Attention
+    attention_layer = MultiHeadAttention(num_heads=num_heads, key_dim=16)
+    attention_output = attention_layer(
+        query=z_behavior_exp,
+        value=z_physio_exp,
+        key=z_physio_exp
+    )
 
-# Cross Attention
-attention_layer = MultiHeadAttention(num_heads=num_heads, key_dim=16)
-attention_output = attention_layer(
-    query=z_behavior_exp,
-    value=z_physio_exp,
-    key=z_physio_exp
-)
+    # Residual connection
+    attention_output = Lambda(lambda x: x[0] + x[1])(
+        [attention_output, z_behavior_exp]
+    )
+    attention_output = LayerNormalization()(attention_output)
 
-# Residual
-attention_output = Lambda(lambda x: x[0] + x[1])([attention_output, z_behavior_exp])
-attention_output = LayerNormalization()(attention_output)
+    # Remove sequence dimension
+    attention_output = Lambda(lambda x: tf.squeeze(x, axis=1))(attention_output)
 
-# Remove sequence dimension safely
-attention_output = Lambda(lambda x: tf.squeeze(x, axis=1))(attention_output)
     # Fusion
     fusion = Concatenate()([z_behavior, z_physio, attention_output])
     fusion = Dense(64, activation='relu')(fusion)
@@ -56,7 +59,7 @@ attention_output = Lambda(lambda x: tf.squeeze(x, axis=1))(attention_output)
     # Classification Output
     class_output = Dense(3, activation='softmax', name='classification')(fusion)
 
-    # HLI Regression Output
+    # Regression Output
     hli_output = Dense(1, activation='linear', name='hli')(fusion)
 
     model = Model(
